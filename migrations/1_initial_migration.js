29ad2223708fd09f03f5a8d43bffd780848b0687
foomin18 <119961541+foomin18@users.noreply.github.com>
2023-12-06 19:45:19 +0900

    Delete migrations directory

--- migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-undef
-const Migrations = artifacts.require('Migrations');
-
-module.exports = function(deployer) { //deploy the migration contract
-    deployer.deploy(Migrations);  //transactionを更新することでスマートコントラクトを更新する
-};
--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-undef
-const SmartHotel = artifacts.require('SmartHotel');
-
-module.exports = async function(deployer, network, accounts) {
-    await deployer.deploy(SmartHotel, '10000000000000000', '10');
-    //const hotel = await SmartHotel.deployed();
-}
